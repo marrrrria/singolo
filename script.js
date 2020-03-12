eac8b532343eff2a1c6ac0b3a2246dd96bf18a0a
Masha <valzyr48@gmail.com>
2020-03-12 16:36:41 +0300

    feat: add functional part for iphone button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const MENU = document.querySelector(".navigation");
 const FORM_SUBMIT = document.getElementById("submit");
 const CLOSE_BUTTON = document.getElementById("close-btn");
+const TURN_OF_VERTICAL_BUTTON = document.getElementById("iphoneV_button");
+const TURN_OF_HORIZONTAL_BUTTON = document.getElementById("iphoneH_button");
 
 MENU.addEventListener("click", event => {
   if (event.target.tagName !== "UL") {
@@ -36,4 +38,11 @@ CLOSE_BUTTON.addEventListener("click", event => {
   document.getElementById("result-comment").innerText = "";
   document.getElementById("result-subject").innerText = "";
   document.getElementById("message-block").classList.add("hidden");
+});
+
+TURN_OF_VERTICAL_BUTTON.addEventListener("click", event => {
+  document.getElementById("iphoneV_turn_of").classList.toggle("hidden");
+});
+TURN_OF_HORIZONTAL_BUTTON.addEventListener("click", event => {
+  document.getElementById("iphoneH_turn_of").classList.toggle("hidden");
 });
